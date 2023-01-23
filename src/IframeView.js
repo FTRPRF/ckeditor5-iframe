5aa483842ca06d50d03f88b2f1e225a733dd3ef9
Badlapje <koen@cornelis.it>
2023-01-23 13:44:58 +0100

    Refactor view-code

--- src/IframeView.js
+++ src/IframeView.js
@@ -23,101 +23,8 @@ export default class FormView extends View {
 	constructor( locale, t ) {
 		super( locale );
 
-		this.t = t;
-		this.focusTracker = new FocusTracker();
-		this.keystrokes = new KeystrokeHandler();
-
-		this.urlInput = this._createInput( t( 'URL' ), 'ck ck-iframe-url' );
-		this.widthInput = this._createInput(
-			t( 'Width' ),
-			'ck ck-iframe-width',
-			t( 'WidthHeightInfo' )
-		);
-		this.heightInput = this._createInput(
-			t( 'Height' ),
-			'ck ck-iframe-height',
-			t( 'WidthHeightInfo' )
-		);
-		this.nameInput = this._createInput(
-			t( 'Name' ),
-			'ck ck-iframe-name',
-			t( 'NameInfo' )
-		);
-		this.advisoryTitleInput = this._createInput(
-			t( 'Title' ),
-			'ck ck-iframe-advisorytitle',
-			t( 'TitleInfo' )
-		);
-		this.longDescriptionInput = this._createInput(
-			t( 'LongDesc' ),
-			'ck ck-iframe-longdesc',
-			t( 'LongDescInfo' )
-		);
-		this.alignmentDropdown = this._createDropdown( t( 'Alignment' ), [
-			{ icon: icons.alignLeft, text: t( 'left' ), className: 'ck ck-iframe-alignleft' },
-			{ icon: icons.alignCenter, text: t( 'middle' ), className: 'ck ck-iframe-alignmiddle' },
-			{ icon: icons.alignRight, text: t( 'right' ), className: 'ck ck-iframe-alignright' }
-		], 'ck ck-iframe-alignment' );
-		this.showScrollbarsToggle = this._createCheckbox( t( 'Scrollbars' ), 'ck ck-iframe-scrollbars', 'showScrollbars' );
-		this.showBorderToggle = this._createCheckbox( t( 'Borders' ), 'ck ck-iframe-borders', 'showBorders' );
-		this.okButton = this._createButton( t( 'OK' ), icons.check, 'ck-iframe-button-ok ck-button-save' );
-		this.cancelButton = this._createButton( t( 'Cancel' ), icons.cancel, 'ck-iframe-button-cancel ck-button-cancel' );
-
-		// Submit type of the button will trigger the submit event on entire form when clicked
-		// (see submitHandler() in render() below).
-		this.okButton.type = 'submit';
-
-		// Delegate ButtonView#execute to FormView#cancel.
-		this.cancelButton.delegate( 'execute' ).to( this, 'cancel' );
-		this.okButton.delegate( 'execute' ).to( this, 'submit' );
-
-		this.secondRow = this.createCollection( [
-			this.widthInput,
-			this.heightInput,
-			this.alignmentDropdown
-		] );
-
-		this.thirdRow = this.createCollection( [
-			this.showScrollbarsToggle,
-			this.showBorderToggle
-		] );
-
-		this.fourthRow = this.createCollection( [
-			this.nameInput,
-			this.advisoryTitleInput
-		] );
-
-		this.buttonRow = this.createCollection( [
-			this.okButton,
-			this.cancelButton
-		] );
-
-		this.childViews = this.createCollection( [
-			this.urlInput,
-			this.widthInput,
-			this.heightInput,
-			this.alignmentDropdown,
-			this.showScrollbarsToggle,
-			this.showBorderToggle,
-			this.nameInput,
-			this.advisoryTitleInput,
-			this.longDescriptionInput,
-			this.okButton,
-			this.cancelButton
-		] );
-
-		this._focusCycler = new FocusCycler( {
-			focusables: this.childViews,
-			focusTracker: this.focusTracker,
-			keystrokeHandler: this.keystrokes,
-			actions: {
-				// Navigate form fields backwards using the Shift + Tab keystroke.
-				focusPrevious: 'shift + tab',
-
-				// Navigate form fields forwards using the Tab key.
-				focusNext: 'tab'
-			}
-		} );
+		this._addFields( t );
+		this._ensureButtonsWork();
 
 		this.setTemplate( {
 			tag: 'div',
@@ -160,38 +67,42 @@ export default class FormView extends View {
 		} );
 	}
 
-	render() {
-		super.render();
+	_addFields( t ) {
+		this._focusCycler = this._createFocusCycler();
 
-		submitHandler( {
-			view: this
-		} );
+		/**
+		 * An array of form validators used by {@link #isValid}.
+		 *
+		 * @readonly
+		 * @protected
+		 * @member {Array.<Function>}
+		 */
+		this._validators = [];
 
-		this.childViews._items.forEach( view => {
-			// Register the view in the focus tracker.
-			this.focusTracker.add( view.element );
-		} );
+		this.advisoryTitleInput = null;
+		this.alignment = null;
+		this.alignmentDropdown = null;
+		this.buttonRow = null;
+		this.cancelButton = null;
+		this.childViews = null;
+		this.heightInput = null;
+		this.focusTracker = new FocusTracker();
+		this.fourthRow = null;
+		this.keystrokes = new KeystrokeHandler();
+		this.longDescriptionInput = null;
+		this.nameInput = null;
+		this.okButton = null;
+		this.secondRow = null;
+		this.showBorderToggle = true;
+		this.showScrollbarsToggle = true;
+		this.t = t;
+		this.thirdRow = null;
+		this.urlInput = null;
+		this.widthInput = null;
 
-		// Start listening for the keystrokes coming from #element.
-		this.keystrokes.listenTo( this.element );
-	}
-
-	destroy() {
-		super.destroy();
-
-		this.focusTracker.destroy();
-		this.keystrokes.destroy();
-	}
-
-	focus() {
-		// If the abbreviation text field is enabled, focus it straight away to allow the user to type.
-		if ( this.urlInput.isEnabled ) {
-			this.urlInput.focus();
-		}
-		// Focus the abbreviation title field if the former is disabled.
-		else {
-			this.widthInput.focus();
-		}
+		this._createFormFields();
+		this._createChildViews();
+		this._createRows();
 	}
 
 	_createButton( label, icon, className ) {
@@ -270,15 +181,178 @@ export default class FormView extends View {
 		dropdown.render();
 		return dropdown;
 	}
+
+	_createFormFields() {
+		const t = this.t;
+
+		this.urlInput = this._createInput( t( 'URL' ), 'ck ck-iframe-url' );
+		this.widthInput = this._createInput(
+			t( 'Width' ),
+			'ck ck-iframe-width',
+			t( 'WidthHeightInfo' )
+		);
+		this.heightInput = this._createInput(
+			t( 'Height' ),
+			'ck ck-iframe-height',
+			t( 'WidthHeightInfo' )
+		);
+		this.nameInput = this._createInput(
+			t( 'Name' ),
+			'ck ck-iframe-name',
+			t( 'NameInfo' )
+		);
+		this.advisoryTitleInput = this._createInput(
+			t( 'Title' ),
+			'ck ck-iframe-advisorytitle',
+			t( 'TitleInfo' )
+		);
+		this.longDescriptionInput = this._createInput(
+			t( 'LongDesc' ),
+			'ck ck-iframe-longdesc',
+			t( 'LongDescInfo' )
+		);
+		this.alignmentDropdown = this._createDropdown( t( 'Alignment' ), [
+			{ icon: icons.alignLeft, text: t( 'left' ), className: 'ck ck-iframe-alignleft' },
+			{ icon: icons.alignCenter, text: t( 'middle' ), className: 'ck ck-iframe-alignmiddle' },
+			{ icon: icons.alignRight, text: t( 'right' ), className: 'ck ck-iframe-alignright' }
+		], 'ck ck-iframe-alignment' );
+		this.showScrollbarsToggle = this._createCheckbox( t( 'Scrollbars' ), 'ck ck-iframe-scrollbars', 'showScrollbars' );
+		this.showBorderToggle = this._createCheckbox( t( 'Borders' ), 'ck ck-iframe-borders', 'showBorders' );
+		this.okButton = this._createButton( t( 'OK' ), icons.check, 'ck-iframe-button-ok ck-button-save' );
+		this.cancelButton = this._createButton( t( 'Cancel' ), icons.cancel, 'ck-iframe-button-cancel ck-button-cancel' );
+	}
 	_createInput( label, className, infoText ) {
 		const labeledInput = new LabeledFieldView( this.locale, createLabeledInputText );
 
 		labeledInput.label = label;
 		labeledInput.class = className;
+
 		if ( infoText ) {
 			labeledInput.infoText = infoText;
 		}
 
 		return labeledInput;
+	}
+
+	_createChildViews() {
+		this.childViews = this.createCollection( [
+			this.urlInput,
+			this.widthInput,
+			this.heightInput,
+			this.alignmentDropdown,
+			this.showScrollbarsToggle,
+			this.showBorderToggle,
+			this.nameInput,
+			this.advisoryTitleInput,
+			this.longDescriptionInput,
+			this.okButton,
+			this.cancelButton
+		] );
+	}
+
+	_createFocusCycler() {
+		return new FocusCycler( {
+			focusables: this.childViews,
+			focusTracker: this.focusTracker,
+			keystrokeHandler: this.keystrokes,
+			actions: {
+				// Navigate form fields backwards using the Shift + Tab keystroke.
+				focusPrevious: 'shift + tab',
+
+				// Navigate form fields forwards using the Tab key.
+				focusNext: 'tab'
+			}
+		} );
+	}
+
+	_createRows() {
+		this.secondRow = this.createCollection( [
+			this.widthInput,
+			this.heightInput,
+			this.alignmentDropdown
+		] );
+
+		this.thirdRow = this.createCollection( [
+			this.showScrollbarsToggle,
+			this.showBorderToggle
+		] );
+
+		this.fourthRow = this.createCollection( [
+			this.nameInput,
+			this.advisoryTitleInput
+		] );
+
+		this.buttonRow = this.createCollection( [
+			this.okButton,
+			this.cancelButton
+		] );
+	}
+
+	_ensureButtonsWork() {
+		// Submit type of the button will trigger the submit event on entire form when clicked
+		// (see submitHandler() in render() below).
+		this.okButton.type = 'submit';
+
+		// Delegate ButtonView#execute to FormView#cancel.
+		this.cancelButton.delegate( 'execute' ).to( this, 'cancel' );
+		this.okButton.delegate( 'execute' ).to( this, 'submit' );
+	}
+	destroy() {
+		super.destroy();
+
+		this.focusTracker.destroy();
+		this.keystrokes.destroy();
+	}
+
+	focus() {
+		// If the abbreviation text field is enabled, focus it straight away to allow the user to type.
+		if ( this.urlInput.isEnabled ) {
+			this.urlInput.focus();
+		}
+		// Focus the abbreviation title field if the former is disabled.
+		else {
+			this.widthInput.focus();
+		}
+	}
+
+	isValid() {
+		return true;
+	}
+
+	render() {
+		super.render();
+
+		submitHandler( {
+			view: this
+		} );
+
+		this.childViews._items.forEach( view => {
+			// Register the view in the focus tracker.
+			this.focusTracker.add( view.element );
+		} );
+
+		// Start listening for the keystrokes coming from #element.
+		this.keystrokes.listenTo( this.element );
+	}
+
+	resetFormStatus() {
+		const { t } = this.t;
+		const borderElement = this.showBorderToggle.element;
+		const scrollbarsElement = this.showScrollbarsToggle.element;
+
+		// Clear the input field values and reset the form.
+		this.advisoryTitleInput.fieldView.element.value = '';
+		this.alignmentDropdown.buttonView.label = t( 'Alignment' );
+		this.heightInput.fieldView.element.value = '';
+		this.longDescriptionInput.fieldView.element.value = '';
+		this.nameInput.fieldView.element.value = '';
+		borderElement.setAttribute( 'aria-pressed', 'true' );
+		borderElement.classList.remove( 'ck-off' );
+		borderElement.classList.add( 'ck-on' );
+		scrollbarsElement.setAttribute( 'aria-pressed', 'true' );
+		scrollbarsElement.classList.remove( 'ck-off' );
+		scrollbarsElement.classList.add( 'ck-on' );
+		this.urlInput.fieldView.element.value = '';
+		this.widthInput.fieldView.element.value = '';
 	}
 }
