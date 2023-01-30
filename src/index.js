025d06b02aad46848b79f82c86cac1ad3c11dc29
Jannick Vandaele <info@jannickvandaele.be>
2023-01-30 14:09:36 +0100

    capital

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ckeditor from './../theme/icons/ckeditor.svg';
 
-export { default as Iframe } from './IFrame';
+export { default as Iframe } from './Iframe';
 
 export const icons = {
 	ckeditor
